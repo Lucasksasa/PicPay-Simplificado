9309c7769ce2b36c5d71df1e217ab5ac084ce1f4
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-02-12 09:30:12 -0300

    UserService and Transaction updated

--- src/main/java/com/picpay_simplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpay_simplificado/domain/transaction/Transaction.java
@@ -5,18 +5,22 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity(name = "transactions")
 @Table(name = "transactions")
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    private BigDecimal amount;
     @JoinColumn(name = "sender_id")
     @ManyToOne
     private User sender;
--- src/main/java/com/picpay_simplificado/services/TransactionService.java
+++ src/main/java/com/picpay_simplificado/services/TransactionService.java
@@ -1,10 +1,19 @@
 package com.picpay_simplificado.services;
 
+import com.picpay_simplificado.domain.transaction.Transaction;
+import com.picpay_simplificado.domain.user.User;
 import com.picpay_simplificado.dtos.TransactionDTO;
 import com.picpay_simplificado.repositories.TransactionRepository;
 import com.picpay_simplificado.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 public class TransactionService {
@@ -14,7 +23,39 @@ public class TransactionService {
     @Autowired
     private TransactionRepository repository;
 
-    public void createTransaction(TransactionDTO transaction){
+    @Autowired
+    private RestTemplate restTemplate;
 
+    public void createTransaction(TransactionDTO transaction) throws Exception {
+        User sender = this.userService.findUserById(transaction.senderId());
+        User receiver = this.userService.findUserById(transaction.receiverId());
+
+        userService.validateTransaction(sender, transaction.value());
+
+        boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+        if(!isAuthorized){
+            throw new Exception ("Transação não autorizada");
+        }
+        Transaction newtransaction = new Transaction();
+        newtransaction.setAmount(transaction.value());
+        newtransaction.setSender(sender);
+        newtransaction.setReceiver(receiver);
+        newtransaction.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
+
+        this.repository.save(newtransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+    }
+
+    public Boolean authorizeTransaction (User sender, BigDecimal value){
+       ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://util.devi.tools/api/v2/authorize", Map.class);
+       if(authorizationResponse.getStatusCode() == HttpStatus.OK){
+           String message = (String) authorizationResponse.getBody().get("message");
+           return "Autorizado".equalsIgnoreCase(message);
+       }
+       else return false;
     }
 }
