50dace4a6d4f982170e852604f8c7c0d40ef8667
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-02-12 08:53:17 -0300

    TransactionService created

---
+++ src/main/java/com/picpay_simplificado/services/TransactionService.java
@@ -0,0 +1,19 @@
+package com.picpay_simplificado.services;
+
+import com.picpay_simplificado.repositories.TransactionRepository;
+import com.picpay_simplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+    public void createTransaction(TransactionDTO transaction){
+        
+    }
+}
