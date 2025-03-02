61a772c5bab14579935efe7caaaff177c8d153f1
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-03-02 19:17:50 -0300

    Created NotificationDTO class

---
+++ src/main/java/com/picpay_simplificado/dtos/NotificationDTO.java
@@ -0,0 +1,4 @@
+package com.picpay_simplificado.dtos;
+
+public class NotificationDTO {
+}
--- src/main/java/com/picpay_simplificado/services/NotificationService.java
+++ src/main/java/com/picpay_simplificado/services/NotificationService.java
@@ -1,4 +1,20 @@
 package com.picpay_simplificado.services;
 
+
+import com.picpay_simplificado.domain.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification (User user, String message){
+    String email = user.getEmail();
+
+    ResponseEntity<String> = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class)
+    }
 }
