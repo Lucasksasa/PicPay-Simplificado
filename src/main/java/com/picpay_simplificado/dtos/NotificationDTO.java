77ac9d5cc8be2e18bd6585f16bf58cc59af06efa
Lucas Ferrari <lucasferrari.ti@hotmail.com>
2025-03-02 19:28:33 -0300

    Class NotificationDTO implemented

--- src/main/java/com/picpay_simplificado/dtos/NotificationDTO.java
+++ src/main/java/com/picpay_simplificado/dtos/NotificationDTO.java
@@ -1,4 +1,4 @@
 package com.picpay_simplificado.dtos;
 
-public class NotificationDTO {
+public record NotificationDTO(String email, String message) {
 }
--- src/main/java/com/picpay_simplificado/services/NotificationService.java
+++ src/main/java/com/picpay_simplificado/services/NotificationService.java
@@ -2,7 +2,9 @@ package com.picpay_simplificado.services;
 
 
 import com.picpay_simplificado.domain.user.User;
+import com.picpay_simplificado.dtos.NotificationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,9 +14,15 @@ public class NotificationService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void sendNotification (User user, String message){
+    public void sendNotification (User user, String message) throws Exception {
     String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
-    ResponseEntity<String> = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class)
+    ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://util.devi.tools/api/v1/notify", notificationRequest, String.class);
+
+    if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+        System.out.println("Erro ao enviar notificação");
+        throw new Exception("Serviço de notificação está fora do ar");
+    }
     }
 }
